383888e40779001526e4ebb7c5fa75f217b51c80
DGR22419 <75600994+dev22419@users.noreply.github.com>
2022-09-30 18:22:46 +0530

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,31 @@
+def display(a , s):
+    print("\n################################### BILLS #####################################")
+    print("name\t\t\tquantity\t\tprice")
+    print(a)
+    print("##############################################################################")
+    print("total = " , s)
+    c = input()
+
+def main():
+    i = 1
+    t = 0
+    x = ""
+    while i < 100 :
+        try:
+            print()
+            print("##############################################################################")
+            print("item no . : " , i)
+            print()
+            n = input("name : ")
+            q = int(input("quantity : "))
+            p = float(input("price : "))
+            t = (q*p) + t
+            q = str(q)
+            p = str(p)
+            x = x + "\n" + n[:5] + "\t\t\t" + q + "\t\t\t" + p
+            i = i + 1
+        except (KeyboardInterrupt):
+            i = 101
+            display(x , t)
+
+main()
