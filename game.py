26e268d6ed707e8b4894b1d8f43c53e4673a96b8
dev22419 <dev22419@gmail.com>
2022-10-03 16:07:21 +0530

    3/10/22

--- game.py
+++ game.py
@@ -5,7 +5,25 @@ import random
 def exit():
     print("bye , have a great time .")
 
-    
+def win(x):
+    print()
+    o = input("do you want to play one more game ? (y , n) : ")
+    if o == "y":
+        main()
+    else :
+        exit()
+
+def lose( i ):
+    print()
+
+    print("you lose the correct answer is" , i)
+    print()
+    o = input("do you want to play one more game ? (y , n) : ")
+    if o == "y":
+        main()
+    else :
+        exit()
+
 def main():
     x = random.randrange(1 , 100)
     print("you have 10 chance to guess the the number .")
@@ -16,26 +34,19 @@ def main():
             print("chance :" , i)
             c = int(input("guess a number : "))
             i = i + 1
+            if c == x :
+                print("hurry , you guessed the correct number .")
+                win(x)
+            elif c < x :
+                print("try guessing a higher value .")
+            elif c > x:
+                print("try guessing a lower number .")
+            else :
+                print("somethimg went wrong .")
+
         except(ValueError):
             print("please , type a number between 1 to 100 .")
 
-        if c == x :
-            print("hurry , you guessed the correct number .")
-        elif c < x :
-            print("try guessing a higher value .")
-        elif c > x:
-            print("try guessing a lower number .")
-        else :
-            print("somethimg went wrong .")
-
-    print()
-
-    print("you lose the correct answer is" , i)
-    print()
-    o = input("do you want to play one more game ? (y , n) : ")
-    if o == "y":
-        main()
-    else :
-        exit()
+    lose(x)
 
 main()
