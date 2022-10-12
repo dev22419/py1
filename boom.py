601ece175520aedd6505fa4318eae34c63d1d827
d22419 <dev22419@gmail.com>
2022-10-12 20:10:54 +0530

    12/10/22/20/10

---
+++ boom.py
@@ -0,0 +1,45 @@
+# code by : dev patel
+# https://github.com/dev22419/
+# let's start
+
+# importing modules
+import pyautogui
+import time
+
+# booming function
+def boom( t , w , n):
+    print("waiting for" , w , "seconds ....")
+    print("open whatsapps as soon  as possible .....")
+    time.sleep(w)
+    i = 1
+    while i <= n :
+        pyautogui.typewrite(t)
+        time.sleep(1)
+        pyautogui.press("enter")
+        i = i + 1
+        
+
+# main fuction
+def main():
+    print("welcome\n")
+
+    t = input("enter the text : ")
+    
+    try :
+        w = int(input("waiting time : "))
+    except (ValueError):
+        print("please type a number not string .")
+        time.sleep(2)
+        main()
+
+    try :
+        n = int(input("boom number : "))
+    except (ValueError):
+        print("please type a number not string .")
+        time.sleep(2)
+        main()
+
+    boom( t , w , n)
+
+
+main()
